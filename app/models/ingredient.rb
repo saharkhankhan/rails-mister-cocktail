0ede0630faa6b0b78627f4e4e88a8ef47afcff60
sahar khan <saharkhan1911@gmail.com>
2019-02-21 14:23:51 +0000

    nested doses route added inside cockrail route

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,7 +2,4 @@ class Ingredient < ApplicationRecord
   has_many :doses
 
   validates :name, presence: true, uniqueness: true
-
-  # You can't delete an ingredient if it used by at least one cocktail.
-
 end
