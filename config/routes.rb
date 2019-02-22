e4dbc55764892b472297464ad955c83c43f33f10
sahar khan <saharkhan1911@gmail.com>
2019-02-22 11:53:13 +0000

    added root to cocktails_path and updated it in the show page back button link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
+
+  root to: 'cocktails#index'
 end
