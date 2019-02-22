5e8cc5e89a7046ff3aa818cd63b4da8d023c4a3a
sahar khan <saharkhan1911@gmail.com>
2019-02-22 17:14:13 +0000

    added photo presence validator in new form and added margin bottom to card on home page

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,7 @@ class Cocktail < ApplicationRecord
   has_many :doses
   has_many :ingredients, through: :doses, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
   mount_uploader :photo, PhotoUploader
 
 end
